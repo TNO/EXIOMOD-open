aef476a67353e40fe621badf7e04a2a9d77ee157
Tatyana Bulavskaya <tatyana.bulavskaya@tno.nl>
2015-08-07 11:16:55 +0200

    Adjust make_project_structure.bat

--- make_project_structure.bat
+++ make_project_structure.bat
@@ -13,10 +13,10 @@ xcopy documentation\templates\Eldata.xlsx project_%ftitle%\00-principal\data
 mkdir project_%ftitle%\00-principal\scr
 xcopy documentation\templates\run_simulation.gms project_%ftitle%\00-principal\scr
 
-mkdir project_%ftitle%\##-scenario#
-mkdir project_%ftitle%\##-scenario#\sets
-mkdir project_%ftitle%\##-scenario#\sets\aggregation
-mkdir project_%ftitle%\##-scenario#\data
-mkdir project_%ftitle%\##-scenario#\scr
+mkdir project_%ftitle%\extradata
+mkdir project_%ftitle%\extradata\sets
+mkdir project_%ftitle%\extradata\sets\aggregation
+mkdir project_%ftitle%\extradata\data
+mkdir project_%ftitle%\extradata\scr
 
 :End
